24505d0ca162a19f31058f557dbf77f9886585c0
Jinsoo89 <hello.python89@gmail.com>
2017-04-03 23:22:59 +0900

    post-setting - diary와 post관련 serializers작성

---
+++ django_app/diary/serializers.py
@@ -0,0 +1,51 @@
+from rest_framework import serializers
+
+from diary.models import Post, Diary, PostPhoto
+from user.serializers import UserSerializer
+
+
+class DiarySerializer(serializers.ModelSerializer):
+    author = UserSerializer(read_only=True)
+    post_list = PostSerializer(many=True, read_only=True, source='post_set')
+
+    class Meta:
+        model = Diary
+        fields = (
+            'pk',
+            'author',
+            'post_list',
+            'created_date',
+        )
+        read_only_fields = (
+            'created_date',
+        )
+
+
+"""
+메타 클래스에 fields값을 pk로 줄지 diary로 줄지 고민됩니다.
+"""
+
+
+class PostSerializer(serializers.ModelSerializer):
+    photo_list = PostPhotoSerializer(many=True, read_only=True,
+                                     source='postphoto_set')
+
+    class Meta:
+        model = Post
+        fields = (
+            'diary',
+            'photo_list',
+            'created_date',
+        )
+        read_only_fields = (
+            'created_date',
+        )
+
+
+class PostPhotoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PostPhoto
+        fields = (
+            'post',
+            'photo',
+        )
