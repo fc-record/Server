a935feaefdf2c6dbab8e4f9a2cfc68b5768839c2
andrew-Woo9 <woo95kil@gmail.com>
2017-04-03 23:46:16 +0900

    RDS modify

--- django_app/config/settings.py
+++ django_app/config/settings.py
@@ -14,7 +14,7 @@ import os
 
 DEBUG = os.environ.get('MODE') == 'DEBUG'
 STORAGE_S3 = os.environ.get('STORAGE') == 'S3' or DEBUG is False
-DB_RDS = os.environ.get('DB') == 'RDS'
+# DB_RDS = os.environ.get('DB') == 'RDS'
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -33,15 +33,26 @@ for key, key_dict in CONFIG_COMMON_FILE.items():
     for inner_key, inner_key_dict in key_dict.items():
         CONFIG_FILE[key][inner_key] = inner_key_dict
 
+# TEMPLATES
+TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
+
+# STATIC
+STATIC_URL = '/static/'
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+    STATIC_DIR,
+]
+
 # AWS S3 Setting
 AWS_ACCESS_KEY_ID = CONFIG_FILE['aws']['access_key_id']
 AWS_SECRET_ACCESS_KEY = CONFIG_FILE['aws']['secret_access_key']
+
+AWS_S3_HOST = '{}.s3.amazonaws.com'.format(CONFIG_FILE['aws']['s3_region'])
 AWS_S3_SIGNATURE_VERSION = CONFIG_FILE['aws']['s3_signature_version']
 AWS_STORAGE_BUCKET_NAME = CONFIG_FILE['aws']['s3_storage_bucket_name']
 AWS_S3_CUSTOM_DOMAIN = '{}.s3.amazonaws.com'.format(AWS_STORAGE_BUCKET_NAME)
 
 # Static Setting
-STATIC_DIR = os.path.join(BASE_DIR, 'static')
 if STORAGE_S3:
     STATICFILES_STORAGE = 'config.storages.StaticStorage'
     STATICFILES_LOCATION = 'static'
@@ -57,26 +68,14 @@ if STORAGE_S3:
         mediafiles_location=MEDIAFILES_LOCATION,
     )
 else:
-    STATIC_URL = '/static/'
     STATIC_ROOT = os.path.join(ROOT_DIR, 'static_root')
+    STATIC_URL = '/static/'
     MEDIA_URL = '/media/'
     MEDIA_ROOT = os.path.join(ROOT_DIR, 'media')
 
-# STATIC
-# BOWER_DIR = os.path.join(ROOT_DIR, 'bower_components')
-STATICFILES_DIRS = [
-    STATIC_DIR,
-]
-
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = CONFIG_FILE['django']['secret-key']
-
-# SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = True
 
 ALLOWED_HOSTS = CONFIG_FILE['django']['allowed-hosts']
 
@@ -89,6 +88,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'storages',
     'rest_framework',
     'rest_framework.authtoken',
     'rest_auth',
@@ -182,4 +183,3 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
-
