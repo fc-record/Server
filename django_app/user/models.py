01776e1010deac7c0cf7bc43f18bce2485a65da7
BestShes <qkrdngus16@gmail.com>
2017-04-04 20:06:06 +0900

    임시로 nickname이 Unique하지 않도록 수정

--- django_app/user/models.py
+++ django_app/user/models.py
@@ -21,7 +21,7 @@ class Member(AbstractUser):
         ('FACEBOOK', 'facebook'),
         ('NORMAL', 'normal')
     )
-    nickname = models.CharField(max_length=20, unique=True)
+    nickname = models.CharField(max_length=20)
     user_type = models.CharField(max_length=8, choices=USER_TYPE_CHOICES, default='normal')
 
     objects = MemberManager()
