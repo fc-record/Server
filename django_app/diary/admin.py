af0203bacc91042d4e37b604f9c6334ad9ab5e42
andrew-Woo9 <woo95kil@gmail.com>
2017-04-08 11:01:00 +0900

    admin add post_photo

--- django_app/diary/admin.py
+++ django_app/diary/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from diary.models import Diary, Post
+from diary.models import Diary, Post, PostPhoto
 
 admin.site.register(Diary)
 admin.site.register(Post)
+admin.site.register(PostPhoto)
