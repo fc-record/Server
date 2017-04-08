346350a3a67454c10253b3674f1dce62467725a7
andrew-Woo9 <woo95kil@gmail.com>
2017-04-08 10:34:57 +0900

    url static path modify

--- django_app/config/urls.py
+++ django_app/config/urls.py
@@ -46,6 +46,6 @@ if settings.DEBUG:
     ]
 
 urlpatterns += static(
-    settings.MEDIA_URL,
-    document_root=settings.MEDIA_ROOT,
+    settings.STATICFILES_LOCATION,
+    document_root=settings.STATICFILES_LOCATION,
 )
