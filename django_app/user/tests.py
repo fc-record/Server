fe95ebab9b5b71ab42c0f94c8e8893053ac2a09d
BestShes <qkrdngus16@gmail.com>
2017-04-27 18:59:53 +0900

    User test 추가 및 google access_token ios버전 검사 추가

--- django_app/user/tests.py
+++ django_app/user/tests.py
@@ -1,24 +1,24 @@
 from django.test import LiveServerTestCase
 from rest_framework import status
+from rest_framework_temporary_tokens.models import TemporaryToken
 
 
 class UserTest(LiveServerTestCase):
-    url = 'http://localhost:8000'
+    url = 'http://api.studydev.kr'
     username = ['testuser1', 'testuser1@gmail.com']
     password = 'testpassword1'
     user_type = ['NORMAL', 'GOOGLE', 'FACEBOOK']
     nickname = 'testnickname'
 
     def create_user(self):
-        url = self.url + '/api/users/'
+        url = self.url + '/users/'
         user = self.client.post(url, data={'username': self.username[1],
                                            'password': self.password,
-                                           'user_type': self.user_type[0],
-                                           'nickname': self.nickname})
+                                           'user_type': self.user_type[0]})
         return user
 
     def test_create_user(self):
-        url = self.url + '/api/users/'
+        url = self.url + '/users/'
 
         # 일반 계정(user_type = NORMAL)의 username이 email형식으로 작성 되었을 경우
         response = self.client.post(url, data={'username': self.username[1],
@@ -44,29 +44,37 @@ class UserTest(LiveServerTestCase):
         url = self.url + '/user/login/'
         # 정상적인 로그인
         response = self.client.post(url, data={'username': self.username[1],
-                                               'password': self.password})
+                                               'password': self.password,
+                                               'user_type': self.user_type[0]})
         self.assertEqual(status.HTTP_200_OK, response.status_code)
         self.assertIn('key', response.data.keys())
         self.assertIn('user', response.data.keys())
 
         # username or password가 틀렸을 경우
         response = self.client.post(url, data={'username': self.username[0],
-                                               'password': self.password})
+                                               'password': self.password,
+                                               'user_type': self.user_type[0]})
         self.assertEqual(status.HTTP_401_UNAUTHORIZED, response.status_code)
         self.assertIn("Username or Password is wrong", response.data['detail'])
+
+        # password가 8자 미만일경우
+        response = self.client.post(url, data={'username': self.username[0],
+                                               'password': '1234567',
+                                               'user_type': self.user_type[0]})
+        self.assertEqual(status.HTTP_400_BAD_REQUEST, response.status_code)
+        self.assertIn("Ensure this field has at least 8 characters.", response.data['password'])
 
         # password를 빈값일 경우
         response = self.client.post(url, data={'username': self.username[1],
                                                'password': ''})
-        self.assertEqual(status.HTTP_401_UNAUTHORIZED, response.status_code)
-        self.assertIn("Username or Password is wrong", response.data['detail'])
+        self.assertEqual(status.HTTP_400_BAD_REQUEST, response.status_code)
+        self.assertIn("This field may not be blank.", response.data['password'])
 
     def test_user_logout(self):
         user = self.create_user()
         url = self.url + '/user/logout/'
         # 정상적인 로그아웃
         response = self.client.post(url, HTTP_AUTHORIZATION='Token {}'.format(user.data['key']))
-        print(response.data)
         self.assertEqual(status.HTTP_200_OK, response.status_code)
         self.assertIn('Logout Succeeded and Token Delete', response.data['detail'])
 
@@ -82,7 +90,14 @@ class UserTest(LiveServerTestCase):
         # 현재 유효한 토큰일 경우
         response = self.client.post(url, data={'key': user.data['key']})
         self.assertEqual(status.HTTP_200_OK, response.status_code)
-        self.assertEqual('valid token', response.data['detail'])
+        self.assertEqual('Valid Token', response.data['detail'])
+
+        # 토큰을 등록되어있지만 유효기간이 지났을 경우
+        token = TemporaryToken.objects.get(key=user.data['key'])
+        token.expire()
+        response = self.client.post(url, data={'key': user.data['key']})
+        self.assertEqual(status.HTTP_401_UNAUTHORIZED, response.status_code)
+        self.assertIn('Token has expired', response.data['detail'])
 
         # 현재 유효하지 않은 토큰일 경우
         response = self.client.post(url, data={'key': 'testtoken'})
@@ -99,13 +114,20 @@ class UserTest(LiveServerTestCase):
         self.assertEqual(status.HTTP_200_OK, response.status_code)
 
         # 변경할 비밀번호를 다르게 입력하거나 8자 미만일 경우
-        response = self.client.post(url, data={'change_password1': '1234qwer', 'change_password2': '1234'},
+        response = self.client.post(url, data={'change_password1': '1234qwer', 'change_password2': '1234qwer1'},
                                     HTTP_AUTHORIZATION='Token {}'.format(user.data['key']))
         self.assertEqual('Password does not match or too short password', response.data['detail'])
 
-    # def test_change_profile_img(self):
-    #     user = self.create_user()
-    #     url = self.url + '/user/changeprofile/'
-    #     response = self.client.post(url, data={'photo': 'iu.jpg'},
-    #                                 HTTP_AUTHORIZATION='Token {}'.format(user.data['key']))
-    #     print(response.data)
+    def test_change_personal(self):
+        user = self.create_user()
+        url = self.url + '/user/changepersonal/'
+
+        response = self.client.post(url, data={'hometown': 'seoul',
+                                               'nickname': 'meme',
+                                               'introduction': 'intro'},
+                                    HTTP_AUTHORIZATION='Token {}'.format(user.data['key']))
+        self.assertEqual(status.HTTP_201_CREATED, response.status_code)
+        self.assertIn('seoul', response.data['user']['hometown'])
+        self.assertIn('meme', response.data['user']['nickname'])
+        self.assertIn('intro', response.data['user']['introduction'])
+
