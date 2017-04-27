b7763ff5f7ed03c765d1a23fbe36a185f1b5be2f
BestShes <qkrdngus16@gmail.com>
2017-04-27 14:36:22 +0900

    API 주소를 /api/users/에서 /users/로 사용하도록 /api부분 삭제, 유저의 마이그레이션 오류 수정

--- django_app/config/urls.py
+++ django_app/config/urls.py
@@ -41,7 +41,7 @@ router.register(
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^api/', include(router.urls, namespace='api')),
+    url(r'^', include(router.urls, namespace='api')),
     url(r'^api-auth/',
         include('rest_framework.urls', namespace='rest_framework')),
     url(r'^user/', include('user.urls')),
